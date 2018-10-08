1c797fb36d12db71fbd954e7554d444724eae214
clevengermatt <clevengermatt@gmail.com>
2018-10-08 19:41:25 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import zupage from "zupage";
+import { Container } from "semantic-ui-react";
+const ReactMarkdown = require("react-markdown");
 
 class App extends Component {
+  state = {
+    author: {},
+    body: "",
+    colorPalette: [],
+    date: "",
+    images: []
+  };
+
+  async componentDidMount() {
+    const postResponse = await zupage.getCurrentPost();
+
+    const date = new Date(
+      postResponse.published_time * 1000
+    ).toLocaleDateString("en-US");
+
+    this.setState({
+      author: postResponse.creator,
+      body: this.formatBody(postResponse.body, postResponse.images),
+      colorPalette: postResponse.page.color_palette,
+      date: date,
+      images: postResponse.images
+    });
+  }
+
+  formatBody = (body, images) => {
+    let newBody = body;
+    let index = 0;
+    images.forEach(function(image) {
+      let strReplace = "{image_" + index + "}";
+      newBody = newBody.replace(strReplace, "![fit](" + image.url + ")");
+      index++;
+    });
+
+    return newBody;
+  };
+
   render() {
+    const { body, colorPalette, images } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Template">
+        <Container text>
+          <ReactMarkdown source={body} />
+        </Container>
       </div>
     );
   }
